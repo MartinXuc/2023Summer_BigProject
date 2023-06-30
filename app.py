bc8d138dcdf112458aee13f42b4f127ad7bf363f
HighVorz <HighVorz@outlook.com>
2023-06-30 20:38:01 +0800

    add image resource request

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, flash, redirect, send_file
 
 
 app = Flask(__name__)
+
+# region img resource
+@app.route('/images/<string:filename>', methods=['GET'])
+def getimage(filename):
+    path = 'static/img/' + filename
+    return send_file(path)
+
+# endregion
 
 
 # region route
